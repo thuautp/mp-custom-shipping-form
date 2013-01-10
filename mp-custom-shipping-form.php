cfa0375e83791d8d4241f8e025f05110adfabc5d
Nathan Onn <nathan@smashingadvantage.com>
2013-01-10 16:14:41 +0800

    Version 1.1.0
    added auto-updater

--- mp-custom-shipping-form.php
+++ mp-custom-shipping-form.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.smashingadvantage.com
 Description: MarketPress Custom Shipping Form Plugin allows you to insert (up to 10) custom fields into the shipping form of your MarketPress sites. This plugin aims to take the pain out of customize your MarketPress shipping form manually and making it very simple for you to customize your shipping form with no technical knowledge needed.
 Author: Nathan Onn - MarketPressThemes.com
 Author URI: http://www.smashingadvantage.com
-Version: 1.0
+Version: 1.1.0
 License: GNU General Public License v2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -79,6 +79,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 				  	add_action( 'mp_new_order' , 'mpcsf_customfield_add_filter_order_notification');
 				  	add_filter( 'mp_shipped_order_notification' , 'mpcsf_customfield_filter_email_order_notification' , 10 , 2);
 
+				  	//updater
+				  	add_action( 'init', array(&$this, 'github_plugin_updater_test_init') );
+
 			    }
 			    
 			    function admin_menu()
@@ -110,6 +113,34 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 				function mpcsf_register_front_end_css() 
 				{
 					wp_enqueue_style('mpcsf-front-end-css', $this->plugin_url . 'css/mpcsf-frontend-css.css', null, null);
+				}
+
+				function github_plugin_updater_test_init() {
+
+					include_once( $this->plugin_path .'inc/mpcsf-updater.php' );
+
+					define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+					if ( is_admin() ) {
+
+						$config = array(
+							'slug' => plugin_basename( __FILE__ ),
+							'proper_folder_name' => 'mp-custom-shipping-form',
+							'api_url' => 'https://api.github.com/repos/nathanonn/mp-custom-shipping-form',
+							'raw_url' => 'https://raw.github.com/nathanonn/mp-custom-shipping-form/master',
+							'github_url' => 'https://github.com/nathanonn/mp-custom-shipping-form',
+							'zip_url' => 'https://github.com/nathanonn/mp-custom-shipping-form/zipball/master',
+							'sslverify' => true,
+							'requires' => '3.3',
+							'tested' => '3.5',
+							'readme' => 'README.md',
+							'access_token' => '',
+						);
+
+						new WP_GitHub_Updater( $config );
+
+					}
+
 				}
 
 			}
